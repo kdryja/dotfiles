eaf2ea84cce21bb52273d2bc673339d9668701e3
devon96 <kdryja@gmail.com>
2019-10-03 19:41:54 +0100

    Initial commit

---
+++ update.sh
@@ -0,0 +1,13 @@
+#/usr/bin/sh
+
+dir=$(dirname "$(realpath "$0")")
+
+cp ~/.tmux.conf $dir
+cp ~/.vimrc $dir
+cp ~/.zprofile $dir
+cp ~/.zshrc $dir
+mkdir -p $dir/.config
+cp -R ~/.config/alacritty $dir/.config
+cp -R ~/.config/sway $dir/.config
+cp -R ~/.config/waybar $dir/.config
+
